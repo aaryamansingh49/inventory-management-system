ebf860411f3346ee24fb4cd702feb22ac2c83203
Aaryaman Singh <aaryamansingh49@gmail.com>
2026-05-06 23:08:35 +0530

    Implemented role-based authorization middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
+const roleMiddleware = require("../middlewares/roleMiddleware");
 const { signup, login } = require("../controllers/authController");
 
 router.post("/signup", signup);
@@ -10,6 +11,12 @@ router.get("/profile", authMiddleware, (req, res) => {
   res.status(200).json({
     message: "Protected route accessed",
     user: req.user,
+  });
+});
+
+router.get("/admin", authMiddleware, roleMiddleware("admin"), (req, res) => {
+  res.status(200).json({
+    message: "Welcome Admin",
   });
 });
 
