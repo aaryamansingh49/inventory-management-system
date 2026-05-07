b1462dce8857adb99f8f18ce1f3aca71ea8a41fe
Aaryaman Singh <aaryamansingh49@gmail.com>
2026-05-07 21:37:03 +0530

    Setup frontend architecture and routing

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const purchaseRoutes = require("./routes/purchaseRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 const invoiceRoutes = require("./routes/invoiceRoutes");
-const apiLimiter = require("./middlewares/rateLimitMiddleware");
+const {authLimiter,apiLimiter,} = require("./middlewares/rateLimitMiddleware");
 
 const app = express();
 app.use(apiLimiter);
@@ -20,13 +20,13 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 //Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/suppliers", supplierRoutes);
+app.use("/api/auth", authLimiter, authRoutes);
+app.use("/api/products", apiLimiter, productRoutes);
+app.use("/api/suppliers", apiLimiter, supplierRoutes);
 app.use("/api/purchases", purchaseRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/dashboard", dashboardRoutes);
-app.use("/api/invoices", invoiceRoutes);
+app.use("/api/orders", apiLimiter, orderRoutes);
+app.use("/api/dashboard", apiLimiter, dashboardRoutes);
+app.use("/api/invoices",  invoiceRoutes);
 
 app.get("/", (req, res) => {
   res.send("Inventory Management API Running...");
--- backend/middlewares/rateLimitMiddleware.js
+++ backend/middlewares/rateLimitMiddleware.js
@@ -1,15 +1,34 @@
 const rateLimit = require("express-rate-limit");
 
-const apiLimiter = rateLimit({
+
+// STRICT AUTH LIMITER
+const authLimiter = rateLimit({
 
   windowMs: 15 * 60 * 1000,
 
-  max: 1000,
+  max: 10,
 
   message: {
-    message: "Too many requests, please try again later"
+    message: "Too many login attempts"
   }
 
 });
 
-module.exports = apiLimiter;
+
+// GENERAL API LIMITER
+const apiLimiter = rateLimit({
+
+  windowMs: 15 * 60 * 1000,
+
+  max: 100,
+
+  message: {
+    message: "Too many requests"
+  }
+
+});
+
+module.exports = {
+  authLimiter,
+  apiLimiter
+};
